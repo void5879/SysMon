131e0292d1e8ce17bc1af69a9c5fc801adafc645
void5879 <sskzm5879@gmail.com>
2025-10-22 22:08:41 +0530

    add data model for the incorporated features

---
+++ frontend/src/main/java/com/sysmon/model/SystemUpdate.java
@@ -0,0 +1,112 @@
+package com.sysmon.model;
+
+public class SystemUpdate {
+  private double cpuUsage;
+  private long memTotal;
+  private long memFree;
+  private long memAvailable;
+  private long buffers;
+  private long cached;
+  private long swapTotal;
+  private long swaptFree;
+  private long netDownSpeed;
+  private long netUpSpeed;
+  private long diskUsed;
+  private long diskTotal;
+
+  public double getCpuUsage() {
+    return cpuUsage;
+  }
+
+  public void setCpuUsage(double cpuUsage) {
+    this.cpuUsage = cpuUsage;
+  }
+
+  public long getMemTotal() {
+    return memTotal;
+  }
+
+  public void setMemTotal(long memTotal) {
+    this.memTotal = memTotal;
+  }
+
+  public long getMemFree() {
+    return memFree;
+  }
+
+  public void setMemFree(long memFree) {
+    this.memFree = memFree;
+  }
+
+  public long getMemAvailable() {
+    return memAvailable;
+  }
+
+  public void setMemAvailable(long memAvailable) {
+    this.memAvailable = memAvailable;
+  }
+
+  public long getBuffers() {
+    return buffers;
+  }
+
+  public void setBuffers(long buffers) {
+    this.buffers = buffers;
+  }
+
+  public long getCached() {
+    return cached;
+  }
+
+  public void setCached(long cached) {
+    this.cached = cached;
+  }
+
+  public long getSwapTotal() {
+    return swapTotal;
+  }
+
+  public void setSwapTotal(long swapTotal) {
+    this.swapTotal = swapTotal;
+  }
+
+  public long getSwaptFree() {
+    return swaptFree;
+  }
+
+  public void setSwaptFree(long swaptFree) {
+    this.swaptFree = swaptFree;
+  }
+
+  public long getNetDownSpeed() {
+    return netDownSpeed;
+  }
+
+  public void setNetDownSpeed(long netDownSpeed) {
+    this.netDownSpeed = netDownSpeed;
+  }
+
+  public long getNetUpSpeed() {
+    return netUpSpeed;
+  }
+
+  public void setNetUpSpeed(long netUpSpeed) {
+    this.netUpSpeed = netUpSpeed;
+  }
+
+  public long getDiskUsed() {
+    return diskUsed;
+  }
+
+  public void setDiskUsed(long diskUsed) {
+    this.diskUsed = diskUsed;
+  }
+
+  public long getDiskTotal() {
+    return diskTotal;
+  }
+
+  public void setDiskTotal(long diskTotal) {
+    this.diskTotal = diskTotal;
+  }
+}
